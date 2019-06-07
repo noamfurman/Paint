d92b0527184aa3941479a14db2059f9eb5c185fa
noamfurman <furmannoam@gmail.com>
2019-06-07 15:59:24 +0300

    made the skeleton

---
+++ painter.js
@@ -0,0 +1,6 @@
+function cordi(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coords = "X coords: " + x + ", Y coords: " + y;
+    document.getElementById("demo").innerHTML = coords;
+  } 
